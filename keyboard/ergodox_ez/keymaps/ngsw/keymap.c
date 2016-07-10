88a8d53773b0a471cd24d53e9288b78488baa21d
ngsw <ngsw@ngsw.jp>
2016-07-10 11:48:50 +0900

    Fix: [],=+,-_,BackSp,L{Alt,Gui}...etc

--- keyboard/ergodox_ez/keymaps/ngsw/keymap.c
+++ keyboard/ergodox_ez/keymaps/ngsw/keymap.c
@@ -10,45 +10,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |   {  |           |  }   |   6  |   7  |   8  |   9  |   0  |   \    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  `/~ |           | '/"  |   6  |   7  |   8  |   9  |   0  |   \    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   -  |           |  +   |   Y  |   U  |   I  |   O  |   P  | BackSp |
- * |--------+------+------+------+------+------|   _  |           |  =   |------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   -  |           |  {   |   Y  |   U  |   I  |   O  |   P  | BackSp |
+ * |--------+------+------+------+------+------|   _  |           |  [   |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
- * |--------+------+------+------+------+------|   ~  |           |  "   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |   `  |           |  '   |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * |--------+------+------+------+------+------|   +  |           |  }   |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |   =  |           |  ]   |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | LAlt | LGui |                                       | Left | Down |  Up  | Right|~FUNCS|
+ *   |      | LAlt | LAlt | LGui | LGui |                                       | Left | Down |  Up  | Right|~FUNCS|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Home | PgUp |       | PgDn | End  |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      | LAlt |       | RAlt |      |      |
- *                                 | Space| -/_  |------|       |------| RGui |Space |
- *                                 |      |      | LAlt |       | RAlt |      |      |
+ *                                 | Space| Back |------|       |------| RGui | -/_  |
+ *                                 |      |   Sp | LAlt |       | RAlt |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LBRC,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_MINS,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV,
-        KC_TRNS,        KC_TRNS,      KC_TRNS,KC_LALT,KC_LGUI,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_EQL,
+        KC_TRNS,        KC_LALT,      KC_LALT,KC_LGUI,KC_LGUI,
                                                       KC_HOME,KC_PGUP,
                                                               KC_LALT,
-                                               KC_SPC,KC_MINS,KC_LALT,
+                                               KC_SPC,KC_BSPC,KC_LALT,
         // right hand
-             KC_RBRC,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSLS,
-             KC_PLUS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSPC,
+             KC_QUOT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSLS,
+             KC_LBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSPC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_ENT,
-             KC_QUOT,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+             KC_RBRC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                KC_LEFT,   KC_DOWN,KC_UP,  KC_RIGHT,         MO(FUNCS),
              KC_PGDN,     KC_END,
              KC_RALT,
-             KC_RALT,KC_RGUI, KC_SPC
+             KC_RALT,KC_RGUI, KC_MINS
     ),
 /* Keymap 1: Function Layer
  *
